16864d8cb816e5bb36b91cdcc1e9ac8efa299280
Monzir <montherrt@gmail.com>
2026-07-04 12:42:32 +0200

    Add files via upload

---
+++ CP/base/api/serializer.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+from base.models import User
+
+class registerserializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+            'confirm_password',
+        )
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError({
+                'confirm_password':'passwords do not match'
+            })
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('confirm_password')
+
+        return User.objects.create_user(
+            first_name = validated_data['first_name'],
+            last_name = validated_data['last_name'],
+            username = validated_data['username'],
+            email = validated_data['email'],
+            password = validated_data['password']
+        )
+
+    
---
+++ CP/base/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .serializer import registerserializer
+
+@api_view(['POST'])
+def register(request):
+    serializer = registerserializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            'message':'user created successfully'
+        },status=201)
+    return Response(serializer.errors,status=400)
+    
+
+
