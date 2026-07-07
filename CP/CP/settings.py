e4059115d096582edaeaaea2d1a10b22ca1dc60c
RedFox199 <montherrt@gmail.com>
2026-07-07 10:31:29 +0200

    added a refresh token and a join date to the /userinfo/

--- CP/CP/settings.py
+++ CP/CP/settings.py
@@ -152,8 +152,8 @@ REST_AUTH = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
 }
 
