a1accbdbbb0734335d7093f8f3e72ceb9d2bfcd6
RedFox199 <montherrt@gmail.com>
2026-07-16 15:38:26 +0300

    added courses, enrolments, categories and edit the user tables

--- CP/base/models.py
+++ CP/base/models.py
@@ -5,10 +5,47 @@ from cloudinary.models import CloudinaryField
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    enrollments = models.ManyToManyField('Enrollments', related_name='users', blank=True)
     
     pfp = CloudinaryField('image', null=True, blank=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
 
-# Create your models here.
+
+class Categories(models.Model):
+    category_name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.category_name
+
+class Courses(models.Model):
+    course_name = models.CharField(max_length=100)
+    course_description = models.TextField()
+    image = CloudinaryField('image', null=True, blank=True)
+    lecturer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='courses')
+    duration = models.IntegerField(help_text="Duration in hours")
+    skill_level = models.CharField(max_length=50, choices=[('Beginner', 'Beginner'), ('Intermediate', 'Intermediate'), ('Advanced', 'Advanced')])
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name='category_courses')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-updated_at', '-created_at']
+        
+    def __str__(self):
+        return self.course_name
+    
+    
+
+    
+class Enrollments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='enrolled_user')
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE, related_name='enrolled_course')
+    enrolled_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'course')
+        
+    def __str__(self):
+        return f"{self.user.username} enrolled in {self.course.course_name}"
